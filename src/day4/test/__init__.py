43227e6b0266d4d985b7bb568d38278ed945881d
iiiGYGYiii <67128969+iiiGYGYiii@users.noreply.github.com>
2024-12-08 22:44:27 -0600

    feat(solutions): day 4

---
+++ src/day4/test/__init__.py
@@ -0,0 +1,25 @@
+import unittest
+
+from os import path
+
+from .. import Solution
+
+CWD = path.dirname(path.realpath(__file__))
+
+
+class TestDay4(unittest.TestCase):
+    def test_part1(self):
+        filepath = path.join(CWD, "test_input.txt")
+        result = Solution(filepath).part1()
+        expected = 18
+        self.assertEqual(result, expected, f"Expected {expected} got {result} instead.")
+
+    def test_part2(self):
+        filepath = path.join(CWD, "test_input.txt")
+        result = Solution(filepath).part2()
+        expected = 9
+        self.assertEqual(result, expected, f"Expected {expected} got {result} instead.")
+
+
+if __name__ == "__main__":
+    unittest.main()
